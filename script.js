086c169cab278e76e5fb7eb1e9e1ffc04f765337
qhProplayerCPP <quanghuy.1334@gmail.com>
2020-11-29 14:58:36 +0700

    truockhimerge

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+$(document).hover(function () {
+  $('[data-toggle="popover"]').popover();
+});
+
 window.addEventListener('load', function (e) {
   let url = `https://5fbfc296fd14be0016749137.mockapi.io/api/caigido`;
   fetch(url)
@@ -9,11 +13,12 @@ window.addEventListener('load', function (e) {
         document.getElementById('chua-may-cai-card').insertAdjacentHTML(
           'beforeend',
           `<div class="cai-card">
+          <a href="#chua-may-cai-card" title="${arr[i].name}" data-toggle="popover" data-trigger="hover" data-content="Cai nay la content se lay tu api ma luoi qua hiiii ^^">
           <img id="hinh-trong-students-viewing" src="${arr[i].ava}">
           <h4 id="ten-trong-students-viewing" class="ten-khoa-hoc">${arr[i].name}</h4>
           <p id="tacgia-trong-students-viewing" class="tac-gia">${arr[i].author}</p>
           <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}$</h4>
-      </div>`
+      </div></a>`
         );
       }
     });
@@ -33,12 +38,12 @@ document.getElementById('input').addEventListener('keypress', function (event) {
           if (arr[i].keyword.includes(input)) {
             document.getElementById('cho-tim-kiem').insertAdjacentHTML(
               'beforeend',
-              `<div class="cai-card">
-            <img id="hinh-trong-students-viewing" src="${arr[i].ava}">
-            <h4 id="ten-trong-students-viewing" class="ten-khoa-hoc">${arr[i].name}</h4>
-            <p id="tacgia-trong-students-viewing" class="tac-gia">${arr[i].author}</p>
-            <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}$</h4>
-        </div>`
+              ` <div class="cai-card">
+              <img id="hinh-trong-students-viewing" src="${arr[i].ava}">
+              <h4 id="ten-trong-students-viewing" class="ten-khoa-hoc">${arr[i].name}</h4>
+              <p id="tacgia-trong-students-viewing" class="tac-gia">${arr[i].author}</p>
+              <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}$</h4>
+          </div>`
             );
           }
         }

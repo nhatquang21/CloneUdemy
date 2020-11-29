94bc59880f077940dbe6c605ffac30e181a8292d
jason1 <quang.thn0837@sinhvien.hoasen.edu.vn>
2020-11-29 14:59:27 +0700

    layout/v3

--- js/index.js
+++ js/index.js
@@ -44,3 +44,32 @@ const addCate = (obj) => {
     }
 }
 addCate(listCate)
+
+  
+window.addEventListener('load', function () {
+    let url = `https://5fbfc296fd14be0016749137.mockapi.io/api/caigido`;
+     fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (arr) {
+        for (let i = 0; i < arr.length; i++) {
+          document.getElementById('chua-may-cai-card').insertAdjacentHTML(
+            'beforeend',
+            `<div class="cai-card">
+            <img id="hinh-trong-students-viewing" src="${arr[i].ava}">
+            <h4 id="ten-trong-students-viewing" class="ten-khoa-hoc">${arr[i].name}</h4>
+            <p id="tacgia-trong-students-viewing" class="tac-gia">${arr[i].author}</p>
+            <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}</h4>
+        </div>`
+          );
+        }
+      });
+  });
+  
+
+let yourCart = []
+let cart = document.getElementById('cart')
+let dataContent = cart.getAttribute('data-content')
+let newContent = `${dataContent} + <br> <a>Keep shopping</a>`
+cart.setAttribute('data-content',newContent )

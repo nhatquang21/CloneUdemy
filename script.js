921f1338be04729f24f7ebbbd3d8bc1529802308
qhProplayerCPP <quanghuy.1334@gmail.com>
2020-11-29 13:36:13 +0700

    cssv2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function (e) {
   let url = `https://5fbfc296fd14be0016749137.mockapi.io/api/caigido`;
-  let get = fetch(url)
+  fetch(url)
     .then(function (response) {
       return response.json();
     })
@@ -12,9 +12,37 @@ window.addEventListener('load', function () {
           <img id="hinh-trong-students-viewing" src="${arr[i].ava}">
           <h4 id="ten-trong-students-viewing" class="ten-khoa-hoc">${arr[i].name}</h4>
           <p id="tacgia-trong-students-viewing" class="tac-gia">${arr[i].author}</p>
-          <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}</h4>
+          <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}$</h4>
       </div>`
         );
       }
     });
+});
+
+document.getElementById('input').addEventListener('keypress', function (event) {
+  if (event.key == 'Enter') {
+    document.getElementById('cho-tim-kiem').innerHTML = '';
+    let input = event.target.value;
+    let url = `https://5fbfc296fd14be0016749137.mockapi.io/api/caigido`;
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (arr) {
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].keyword.includes(input)) {
+            document.getElementById('cho-tim-kiem').insertAdjacentHTML(
+              'beforeend',
+              `<div class="cai-card">
+            <img id="hinh-trong-students-viewing" src="${arr[i].ava}">
+            <h4 id="ten-trong-students-viewing" class="ten-khoa-hoc">${arr[i].name}</h4>
+            <p id="tacgia-trong-students-viewing" class="tac-gia">${arr[i].author}</p>
+            <h4 id="gia-trong-students-viewing" class="gia">${arr[i].price}$</h4>
+        </div>`
+            );
+          }
+        }
+      });
+    event.preventDefault();
+  }
 });
